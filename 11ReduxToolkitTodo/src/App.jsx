2e2167fd6bd53454ae3c3eda5a43f24b586e8356
AmitVerma-01 <amitvermaup57@gmail.com>
2024-02-05 00:05:38 +0530

    complete todo app using redux Toolkit

--- 11ReduxToolkitTodo/src/App.jsx
+++ 11ReduxToolkitTodo/src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+import './App.css'
 
 function App() {
 
   return (
     <>
-      <h2 className='text-3xl bg-pink-800'>redux toolkit</h2>
+      <h2 className='text-3xl'>Redux toolkit</h2>
+      <AddTodo />
+      <Todos />
     </>
   )
 }
