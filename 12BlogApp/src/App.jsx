cb795ec5b0c1254860832f70594605e0d82412ff
AmitVerma-01 <amitvermaup57@gmail.com>
2024-02-05 13:41:14 +0530

    authService

---
+++ 12BlogApp/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+  return (
+    <>
+    <h1 className='text-3xl bg-yellow-200'>hello worold</h1>
+    </>
+  )
+}
+
+export default App
